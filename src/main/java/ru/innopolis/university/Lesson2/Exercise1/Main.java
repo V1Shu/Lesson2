a5360942edc510152bf5379901a4109ba792aa66
V1Shu <vadim_sh@bk.ru>
2020-10-02 00:20:42 +0200

    Exception task

--- src/main/java/ru/innopolis/university/Lesson2/Exercise1/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.innopolis.university.Lesson2.Exercise1;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ src/main/java/ru/innopolis/university/Lesson2/Task1/ThrowException.java
@@ -0,0 +1,8 @@
+package ru.innopolis.university.Lesson2.Task1;
+
+public class ThrowException {
+
+    public static void main(String[] args) {
+        throw new ArithmeticException();
+    }
+}
