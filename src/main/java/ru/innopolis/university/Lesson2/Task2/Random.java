028c7b38ce5cad0da8b7bb41a011e6ba1c6de173
vadim_sh@bk.ru <vadim_sh@bk.ru>
2020-10-02 16:33:41 +0200

    Ready Random Task

--- src/main/java/ru/innopolis/university/Lesson2/Task2/Random.java
+++ src/main/java/ru/innopolis/university/Lesson2/Task2/Random.java
@@ -1,7 +1,31 @@
 package ru.innopolis.university.Lesson2.Task2;
 
+import java.util.Scanner;
+
 public class Random {
+
     public static void main(String[] args) {
 
+        int countOfNumbers = GetCountOfNumbers();
+
+        for (int counter = 0; counter < countOfNumbers; counter++) {
+            double randomNumber = Math.random() * 10;
+            double numberRoot = Math.sqrt(randomNumber);
+            int intForNumberRoot = (int) numberRoot;
+            if (intForNumberRoot * intForNumberRoot == counter) {
+                System.out.println(counter);
+            }
+        }
+    }
+
+    static int GetCountOfNumbers() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type count of numbers");
+        int getCount = scanner.nextInt();
+        if (getCount >= 0) {
+            return getCount;
+        } else {
+            throw new ArithmeticException("Number can't be negative");
+        }
     }
 }
