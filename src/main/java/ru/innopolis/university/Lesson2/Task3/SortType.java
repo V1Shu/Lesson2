5131cbb69dda5185969de0e8d2fedfd3ae40cdfb
V1Shu <vadim_sh@bk.ru>
2020-10-11 12:40:36 +0200

    refactoring ready

--- src/main/java/ru/innopolis/university/Lesson2/Task3/SortType.java
+++
@@ -1,7 +0,0 @@
-package ru.innopolis.university.Lesson2.Task3;
-
-public enum SortType {
-    MANFIRST,
-    OLDERFIRST,
-    ALPHABET
-}
--- src/main/java/ru/innopolis/university/lesson2/task3/Main.java
+++ src/main/java/ru/innopolis/university/lesson2/task3/Main.java
@@ -1,26 +1,40 @@
-package ru.innopolis.university.Lesson2.Task3;
+package ru.innopolis.university.lesson2.task3;
+
+import ru.innopolis.university.lesson2.task3.model.Person;
+import ru.innopolis.university.lesson2.task3.model.SortType;
+import ru.innopolis.university.lesson2.task3.service.serviceMethods;
+import ru.innopolis.university.lesson2.task3.sort.BubbleSort;
+import ru.innopolis.university.lesson2.task3.sort.InsertionSort;
+import ru.innopolis.university.lesson2.task3.sort.SortInterface;
 
 import java.util.Scanner;
 
+/**
+ * @author Shulepov Vadim
+ */
 public class Main {
 
+    /**
+     * Generate Array of Person objects, ask user how to sort it and print result of sort
+     * @param args
+     */
     public static void main(String[] args) {
-        Person[] personArray = new Person[10];
-        Person.FillArray(personArray);
+        Person[] personArray = new Person[10000];
+        serviceMethods.fillArrayOfPerson(personArray);
 
-        PrintResult(personArray);
-        PrintInstructions();
+        printResult(personArray);
+        printInstructions();
 
         Scanner scanner = new Scanner(System.in);
         int select = scanner.nextInt();
         while (select != 0) {
             switch (select) {
-                case 1: CallBubbleSort(personArray.clone(), SortType.MANFIRST); break;
-                case 2: CallInsertionSort(personArray.clone(), SortType.MANFIRST); break;
-                case 3: CallBubbleSort(personArray.clone(), SortType.OLDERFIRST); break;
-                case 4: CallInsertionSort(personArray.clone(), SortType.OLDERFIRST); break;
-                case 5: CallBubbleSort(personArray.clone(), SortType.ALPHABET); break;
-                case 6: CallInsertionSort(personArray.clone(), SortType.ALPHABET); break;
+                case 1: callSort(personArray.clone(), SortType.MANFIRST, new BubbleSort()); break;
+                case 2: callSort(personArray.clone(), SortType.MANFIRST, new InsertionSort()); break;
+                case 3: callSort(personArray.clone(), SortType.OLDERFIRST, new BubbleSort()); break;
+                case 4: callSort(personArray.clone(), SortType.OLDERFIRST, new InsertionSort()); break;
+                case 5: callSort(personArray.clone(), SortType.ALPHABET, new BubbleSort()); break;
+                case 6: callSort(personArray.clone(), SortType.ALPHABET, new InsertionSort()); break;
                 default:
                     System.out.println("Incorrect number");
                     break;
@@ -29,7 +43,10 @@ public class Main {
         }
     }
 
-    private static void PrintInstructions() {
+    /**
+     * print instructions at console
+     */
+    private static void printInstructions() {
         System.out.println("\nType number of sort :");
         System.out.println("Man first : \n1  Bubble sort \n2  Insertion sort");
         System.out.println("Older first : \n3  Bubble sort \n4  Insertion sort");
@@ -37,33 +54,29 @@ public class Main {
         System.out.println("\n0 For Exit");
     }
 
-    private static void CallBubbleSort(Person[] array, SortType type) {
-        BubbleSort sort = new BubbleSort();
+    /**
+     * Call chosen type and direction of sort
+     * @param array Array of Person objects, needed to sort
+     * @param type Direction of sort
+     * @param sort Type of sort
+     */
+    private static void callSort(Person[] array, SortType type, SortInterface sort) {
         long startTime = System.nanoTime();
         switch (type) {
-            case MANFIRST: sort.ManFirst(array); break;
-            case OLDERFIRST: sort.OlderFirst(array); break;
-            case ALPHABET: sort.Alphabet(array); break;
+            case MANFIRST: sort.manFirst(array); break;
+            case OLDERFIRST: sort.olderFirst(array); break;
+            case ALPHABET: sort.alphabet(array); break;
         }
         long estimatedTime = System.nanoTime() - startTime;
-        PrintResult(array);
+        printResult(array);
         System.out.println("Estimated time: " + estimatedTime);
     }
 
-    private static void CallInsertionSort(Person[] array, SortType type) {
-        InsertionSort sort = new InsertionSort();
-        long startTime = System.nanoTime();
-        switch (type) {
-            case MANFIRST: sort.ManFirst(array); break;
-            case OLDERFIRST: sort.OlderFirst(array); break;
-            case ALPHABET: sort.Alphabet(array); break;
-        }
-        long estimatedTime = System.nanoTime() - startTime;
-        PrintResult(array);
-        System.out.println("Estimated time: " + estimatedTime);
-    }
-
-    private static void PrintResult(Person[] array) {
+    /**
+     * Print elements of array
+     * @param array Array of Person objects, needed to print
+     */
+    private static void printResult(Person[] array) {
         System.out.println("-------------------------RESULT---------------------------");
         for (Person person : array) {
             System.out.println(person.toString());
