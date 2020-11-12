a376f3fee0abc0521309ac18054edbe8a931b43c
vadim_sh@bk.ru <vadim_sh@bk.ru>
2020-11-12 11:17:49 +0200

    lesson 8 ready

--- src/main/java/ru/innopolis/university/lesson8/Main.java
+++ src/main/java/ru/innopolis/university/lesson8/Main.java
@@ -1,4 +1,34 @@
 package ru.innopolis.university.lesson8;
 
+import ru.innopolis.university.lesson8.services.ServiceMethods;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * test calculating factorial using threads
+ * @author v.shulepov
+ */
 public class Main {
+    /**
+     * count of threads using for calculate factorial
+     */
+    private static final int COUNT_OF_THREADS = 10;
+    /**
+     * count of elements in integer ArrayList
+     */
+    private static final int COUNT_OF_ARRAY_ELEMENTS = 1000;
+
+    public static void main(String[] args) {
+        ArrayList<Integer> integerArray = new ArrayList<>();
+        ServiceMethods.fillNumberArray(integerArray, COUNT_OF_ARRAY_ELEMENTS);
+        integerArray.forEach(System.out::println);
+        System.out.println("====================================================");
+        ExecutorService executorService = Executors.newFixedThreadPool(COUNT_OF_THREADS);
+        for (int count = 0; count < integerArray.size(); count++) {
+            int finalCount = count;
+            executorService.submit(() -> System.out.println(ServiceMethods.factorial(integerArray.get(finalCount))));
+        }
+    }
 }
--- src/main/java/ru/innopolis/university/lesson8/services/ServiceMethods.java
+++ src/main/java/ru/innopolis/university/lesson8/services/ServiceMethods.java
@@ -1,13 +1,22 @@
 package ru.innopolis.university.lesson8.services;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ServiceMethods {
     private static final Random RANDOM = new Random();
 
-    private static void fillNumberArray(Integer[] integersArray) {
-        for (int count = 0; count < integersArray.length; count++) {
-
+    public static void fillNumberArray(ArrayList<Integer> integersArray, int countOfNumbers) {
+        for (int count = 0; count < countOfNumbers; count++) {
+            integersArray.add(RANDOM.nextInt(99));
         }
+    }
+
+    public static BigInteger factorial(int number) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= number; i++)
+            result = result.multiply(BigInteger.valueOf(i));
+        return result;
     }
 }
