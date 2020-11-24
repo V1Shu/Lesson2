98f6abe1894d410dd7ded259cb19af718011ed6a
vadim_sh@bk.ru <vadim_sh@bk.ru>
2020-11-24 16:42:19 +0200

    get connection but dont message

--- src/main/java/ru/innopolis/university/lesson11/Client/Client.java
+++ src/main/java/ru/innopolis/university/lesson11/Client/Client.java
@@ -4,27 +4,63 @@ import ru.innopolis.university.lesson11.Server.Server;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", Server.SERVER_PORT);
+    private final static Scanner scanner = new Scanner(System.in);
+    private static BufferedReader reader;
+    private static BufferedWriter writer;
 
-        BufferedWriter bufferedWriter =
-                new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        BufferedReader bufferedReader =
-                new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public static void main(String[] args) {
+        new Client();
+    }
 
-        System.out.println("Type your name: ");
-        Scanner scanner = new Scanner(System.in);
-        String message;
+   public Client() {
+       try {
+           Socket socket = new Socket("localhost", Server.SERVER_PORT);
 
-        while (!(message = scanner.nextLine()).isEmpty()) {
-            bufferedWriter.write(message);
-            bufferedWriter.newLine();
-            bufferedWriter.flush();
+           writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+           reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            System.out.println("server echo " + bufferedReader.readLine());
+           System.out.println("Type your name: ");
+           writer.write(scanner.nextLine());
+
+           Resender resend = new Resender();
+           resend.start();
+
+           String message = "";
+           while (!message.equals("exit")) {
+               message = scanner.nextLine();
+               writer.write(message);
+               writer.flush();
+           }
+           resend.setStop();
+       } catch (UnknownHostException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+
+    private class Resender extends Thread {
+
+        private boolean stoped;
+
+        public void setStop() {
+            stoped = true;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (!stoped) {
+                    String str = reader.readLine();
+                    System.out.println(str);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/ru/innopolis/university/lesson11/Server/Server.java
+++ src/main/java/ru/innopolis/university/lesson11/Server/Server.java
@@ -3,24 +3,88 @@ package ru.innopolis.university.lesson11.Server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Server {
     public static final int SERVER_PORT = 5678;
-    private static final ArrayList<Connection> listOfConnections = new ArrayList<>();
+    private static final List<Connection> listOfConnections =
+            Collections.synchronizedList(new ArrayList<>());
+    private static ServerSocket serverSocket;
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+    public static void main(String[] args) {
+        new Server();
+    }
 
-        Socket socket = serverSocket.accept();
-        Connection connection = new Connection(socket);
-        listOfConnections.add(connection);
+    public Server() {
+        try {
+        serverSocket = new ServerSocket(SERVER_PORT);
 
-        String message;
-        while (!"exit".equals(message = connection.getReader().readLine())) {
-            System.out.println("Server read: " + message);
-            connection.getWriter().write("\"" + message + "\" received \n");
-            connection.getWriter().flush();
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            Connection con = new Connection(socket);
+            listOfConnections.add(con);
+
+            con.start();
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class Connection extends Thread {
+        private BufferedReader reader;
+        private BufferedWriter writer;
+        private Socket socket;
+
+        private String name = "";
+
+        public Connection(Socket socket) {
+            this.socket = socket;
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                name = reader.readLine();
+                /*synchronized (listOfConnections) {
+                    Iterator<Connection> iter = listOfConnections.iterator();
+                    while (iter.hasNext()) {
+                        ((Connection) iter.next()).writer.write(name + " cames now");
+                    }
+                }*/
+
+                String str = "";
+                while (true) {
+                    str = reader.readLine();
+                    if (str.equals("exit")) break;
+
+                    synchronized (listOfConnections) {
+                        Iterator<Connection> iter = listOfConnections.iterator();
+                        while (iter.hasNext()) {
+                            Connection currentConnection = iter.next();
+                            currentConnection.writer.write(name + ": " + str);
+                            currentConnection.writer.flush();
+                        }
+                    }
+                }
+
+                /*synchronized (listOfConnections) {
+                    Iterator<Connection> iter = listOfConnections.iterator();
+                    while (iter.hasNext()) {
+                        ((Connection) iter.next()).writer.write(name + " has left");
+                    }
+                }*/
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
