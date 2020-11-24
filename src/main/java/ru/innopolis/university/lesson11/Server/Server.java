a4fcf0ccaf4cde39a1b89f3536ec7cae81463fcc
vadim_sh@bk.ru <vadim_sh@bk.ru>
2020-11-24 14:23:40 +0200

    add listOfConnections

--- src/main/java/ru/innopolis/university/lesson11/Server/Server.java
+++ src/main/java/ru/innopolis/university/lesson11/Server/Server.java
@@ -1,96 +1,23 @@
 package ru.innopolis.university.lesson11.Server;
 
 import java.io.*;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.ArrayList;
 
 public class Server {
     public static final int SERVER_PORT = 5678;
-    private static final Map<Connection, String> connections = new Map<Connection, String>() {
-        @Override
-        public int size() {
-            return this.size();
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return this.size() == 0;
-        }
-
-        @Override
-        public boolean containsKey(Object key) {
-            return false;
-        }
-
-        @Override
-        public boolean containsValue(Object value) {
-            return false;
-        }
-
-        @Override
-        public String get(Object key) {
-            return this.get(key);
-        }
-
-        @Override
-        public String put(Connection key, String value) {
-            return null;
-        }
-
-        @Override
-        public String remove(Object key) {
-            return this.remove(key);
-        }
-
-        @Override
-        public void putAll(Map<? extends Connection, ? extends String> m) {
-
-        }
-
-        @Override
-        public void clear() {
-            this.clear();
-        }
-
-        @Override
-        public Set<Connection> keySet() {
-            return null;
-        }
-
-        @Override
-        public Collection<String> values() {
-            return null;
-        }
-
-        @Override
-        public Set<Entry<Connection, String>> entrySet() {
-            return null;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            return false;
-        }
-
-        @Override
-        public int hashCode() {
-            return 0;
-        }
-    };
+    private static final ArrayList<Connection> listOfConnections = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(SERVER_PORT,0, InetAddress.getByName("localhost"));
+        ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
 
         Socket socket = serverSocket.accept();
         Connection connection = new Connection(socket);
-        //connections.put(connection, connection.getReader().readLine());
+        listOfConnections.add(connection);
 
         String message;
-        while ((message = connection.getReader().readLine()) != null) {
+        while (!"exit".equals(message = connection.getReader().readLine())) {
             System.out.println("Server read: " + message);
             connection.getWriter().write("\"" + message + "\" received \n");
             connection.getWriter().flush();
