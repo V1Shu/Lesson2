9a3f07bff02c392fd7b5137ba95c4280b61be783
vadim_sh@bk.ru <vadim_sh@bk.ru>
2020-12-03 16:52:13 +0200

    circleci test

--- src/main/java/ru/innopolis/university/lesson13/task2/MetaSpace_OutOfMemory.java
+++ src/main/java/ru/innopolis/university/lesson13/task2/MetaSpace_OutOfMemory.java
@@ -1,8 +1,38 @@
 package ru.innopolis.university.lesson13.task2;
 
+import java.util.Base64;
+
 public class MetaSpace_OutOfMemory {
 
     public static void main(String[] args) {
+        String clazzBase64 ="yv66vgAAADcADAEAEm15cGFja2FnZS9NeWNsYXNzMAcAAQEAEGphdmEvbGFuZy9PYmplY3QHAAMBAApTb3VyY2VGaWxlAQANTXljbGFzczAuamF2YQEABjxpbml0PgEAAygpVgwABwAICgAEAAkBAARDb2RlACEAAgAEAAAAAAABAAEABwAIAAEACwAAABEAAQABAAAABSq3AAqxAAAAAAABAAUAAAACAAY=";
 
+        byte[] compiledClazz = Base64.getDecoder().decode(clazzBase64);
+        int classNameLength = Integer.valueOf(compiledClazz[12]);
+
+        MyClassLoader myClassLoader = new MyClassLoader(Thread.currentThread().getContextClassLoader());
+
+        for (int i = 0; ; i++) {
+            byte[] bytes = String.valueOf(i).getBytes();
+            byte[] bytecode = new byte[compiledClazz.length + bytes.length - 1];
+            System.arraycopy(compiledClazz, 0, bytecode, 0, 30);
+            bytecode[12] = (byte) (classNameLength + bytes.length - 1 & 0xFF);
+
+            System.arraycopy(bytes, 0, bytecode, 30, bytes.length);
+            System.arraycopy(compiledClazz, 31, bytecode, 30 + bytes.length, compiledClazz.length - 31);
+
+            String classname = "mypackage.Myclass" + i;
+            Class c = myClassLoader.getClass(classname, bytecode);
+        }
+    }
+
+    public static class MyClassLoader extends ClassLoader {
+        public MyClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        public Class<?> getClass(String name, byte[] code) {
+            return defineClass(name, code, 0, code.length);
+        }
     }
 }
